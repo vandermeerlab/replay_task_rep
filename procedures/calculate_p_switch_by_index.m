b77ea34782994a97c0a5d7c987e9f9eb0d823d5f
transedward <transedward@gmail.com>
2022-03-12 20:25:25 -0500

    Update calculate_p_switch_by_index.m

--- procedures/calculate_p_switch_by_index.m
+++ procedures/calculate_p_switch_by_index.m
@@ -1,48 +1,40 @@
 function [p_switch] = calculate_p_switch_by_index(cfg_in, SWR_data)
 
-% Calculate the probablity of switching across deltas of event index
+% Calculate the probablity of switching across delta event index
+% Takes SWR_data, size of [1, n_SWR_events] whose element is L if > 0, R if
+% < 0
+% Return p_switch, size of [1, devt_max] whose elements containing the
+% proportion of switching between events across delta indices
 
 cfg_def = [];
+cfg_def.devt_max = 50;
 
 cfg = ProcessConfig(cfg_def,cfg_in);
 
-L2_L1_count = 0;
-R2_R1_count = 0;
-R2_L1_count = 0;
-L2_R1_count = 0;
-
-for evt_i = 1:length(SWR_data)-1
-    if ~isnan(SWR_data(evt_i+1)) && ~isnan(SWR_data(evt_i))
-        if SWR_data(evt_i+1) > 0 && SWR_data(evt_i) > 0
-            L2_L1_count = L2_L1_count + 1;
-        elseif SWR_data(evt_i+1) < 0 && SWR_data(evt_i) < 0
-            R2_R1_count = R2_R1_count + 1;
-        elseif SWR_data(evt_i+1) < 0 && SWR_data(evt_i) > 0
-            R2_L1_count = R2_L1_count + 1;
-        elseif SWR_data(evt_i+1) > 0 && SWR_data(evt_i) < 0
-            L2_R1_count = L2_R1_count + 1;
+for d_i = 1:cfg.devt_max
+    p_switch_devt = [];
+    for evt_i = 1:length(SWR_data)-1
+        if evt_i + d_i <= length(SWR_data)
+            SWR_evt1 = SWR_data(evt_i);
+            SWR_evt2 = SWR_data(evt_i + d_i);
+            if ~isnan(SWR_evt1) && ~isnan(SWR_evt2)
+                % Both events are L
+                if SWR_evt1 > 0 && SWR_evt2 > 0
+                    p_switch_devt = [p_switch_devt, 0];
+                % Both events are R
+                elseif SWR_evt1 < 0 && SWR_evt2 < 0
+                    p_switch_devt = [p_switch_devt, 0];
+                % Switch
+                elseif SWR_evt1 < 0 && SWR_evt2 > 0
+                    p_switch_devt = [p_switch_devt, 1];
+                % Switch
+                elseif SWR_evt1 > 0 && SWR_evt1 < 0
+                    p_switch_devt = [p_switch_devt, 1];
+                end
+            end
         end
+        p_switch(d_i) = sum(p_switch_devt) / length(p_switch_devt);
     end
 end
-L1_count = L2_L1_count + R2_L1_count;
-R1_count = L2_R1_count + R2_R1_count;
-
-if L1_count == 0
-    P_R2_L1 = 0;
-    P_L2_L1 = 0;
-else
-    P_R2_L1 = R2_L1_count / L1_count;
-    P_L2_L1 = L2_L1_count / L1_count;
-end
-
-if R1_count == 0
-    P_L2_R1 = 0;
-    P_R2_R1 = 0;
-else
-    P_L2_R1 = L2_R1_count / R1_count;
-    P_R2_R1 = R2_R1_count / R1_count;
-end
-
-p_switch = (P_R2_L1 + P_L2_R1) - (P_L2_L1 + P_R2_R1);
 
 end
