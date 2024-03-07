2a542958a71eeda70104f4f4918c0fca0ae56d16
transedward <hungtu.e.chen@gmail.com>
2024-03-07 17:58:16 -0500

    bunch of style updates

--- simulations/utils/plotting.py
+++ simulations/utils/plotting.py
@@ -2,26 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_var(var_array,
-            parameters,
+            param_labels=['rich', 'lazy'],
+            y_ticks=None,
             y_label="MSE",
-            title="Training loss (Mean Squared Error)"):
+            fig_size=(10, 5)):
   """
   Plot time-varying variables function
 
   Args:
-    title: string
-      Specifies plot title
     var_array: np.ndarray
       Log of variables (ex: MSE loss) per epoch
+    param_labels: list
+      List of parameter labels
 
   Returns:
     Nothing
   """
-  plt.figure(figsize=(10, 5))
-  for p_i, param in enumerate(parameters):
-    plt.plot(np.mean(var_array[p_i, :, :], 0), label=param)
-    plt.xlabel("Epoch")
-    plt.ylabel(y_label)
-    plt.title(title)
-  plt.legend()
-  plt.show()
+  fig, ax = plt.subplots()
+  fig.set_size_inches(fig_size)
+  for p_i, param in enumerate(param_labels):
+    ax.plot(np.mean(var_array[p_i, :, :], 0), label=param)
+    ax.set_xlabel("epoch")
+    ax.set_ylabel(y_label)
+    ax.set_xticks([0, 2500, 5000])
+    if y_ticks:
+        ax.set_yticks(y_ticks)
+    ax.legend()
+  return fig
