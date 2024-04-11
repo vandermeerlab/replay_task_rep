937ef1518f28c60a814437745ea2b13c445e2db3
transedward <hungtu.e.chen@gmail.com>
2024-04-11 00:13:44 -0400

    Add random seed and change task rep layout, fonsize

--- simulations/utils/plotting.py
+++ simulations/utils/plotting.py
@@ -8,6 +8,7 @@ def plot_var(var_array,
             param_labels=['rich', 'lazy'],
             colors = [COLORS['rich'], COLORS['lazy']],
             y_ticks=None,
+            y_tick_labels=[],
             y_label="error",
             fig_size=(10, 5)):
   """
@@ -33,48 +34,42 @@ def plot_var(var_array,
       var = np.convolve(np.mean(var_array[p_i, :, :], 0), np.ones(running_avg)/running_avg, mode='valid')
     else:
       var = np.mean(var_array[p_i, :, :], 0)
-    axis.plot(np.arange(epoch_start, var.shape[0]+epoch_start), var, color=colors[p_i], label=param)
+    axis.plot(np.arange(epoch_start, var.shape[0]+epoch_start), var,
+              color=colors[p_i], linewidth=2, label=param)
 
   axis.set_xlabel("epoch")
   axis.set_ylabel(y_label)
-  axis.set_xticks(np.array([0, 2500, 5000]) + epoch_start)
+  axis.set_xticks(np.array([0, 1250, 2500, 3750, 5000]) + epoch_start)
+  axis.set_xticklabels(np.array([0+epoch_start, None, 2500+epoch_start, None, 5000+epoch_start]))
   if y_ticks:
       axis.set_yticks(y_ticks)
+  if y_tick_labels:
+    axis.set_yticklabels(y_tick_labels)
   axis.legend()
   return fig, axis
 
-def get_task_color(X):
-    colors = []
-    for x1, x2 in X:
-      if x1 == -1. and x2 == 1.:
-           colors.append(COLORS['left_fr'])
-      elif x1 == 1. and x2 == 1.:
-           colors.append(COLORS['right_fr'])
-      elif x1 == -1. and x2 == -1.:
-           colors.append(COLORS['left_wr'])
-      elif x1 == 1. and x2 == -1.:
-           colors.append(COLORS['right_wr'])
-      else:
-           colors.append('grey')
-    return colors
-
-def plot_task_rep(X, y, colors=[], marker_size=200):
+def plot_task_rep(X, y, colors=[], markers=[], marker_size=200):
   """
   Plotting data inputs in the task representation.
   """
   fig, axis = plt.subplots()
-  if not colors:
-     colors = get_task_color(X)
   for i in range(y.shape[0]):
-      marker = '^' if y[i] == 1 else 'v'
-      axis.scatter(X[i, 0], X[i, 1], c = colors[i], marker=marker, s=marker_size)
+      if colors:
+         color = colors[i]
+      else:
+        color = COLORS['pos'] if y[i] == 1 else COLORS['neg']
+      if markers:
+         marker = markers[i]
+      else:
+        marker = 'o' if X[i, 1] == 1 else '^'
+      axis.scatter(X[i, 0], X[i, 1], c = color, marker=marker, s=marker_size)
 
   axis.set_xlim(-1.5, 1.5)
   axis.set_xticks([-1, 1])
   axis.set_xticklabels(['left', 'right'])
   axis.set_ylim(-1.5, 1.5)
   axis.set_yticks([-1, 1])
-  axis.set_yticklabels(['wr', 'fr'])
+  axis.set_yticklabels(['ctx2', 'ctx1'])
 
   return fig, axis
 
@@ -87,8 +82,10 @@ def plot_hidden_in(X, axis):
   axis.set_ylabel('input weights $\it{j}$')
   axis.set_xlim(-0.5, 0.5)
   axis.set_ylim(-0.5, 0.5)
-  axis.set_xticks([-0.4, 0, 0.4])
-  axis.set_yticks([-0.4, 0, 0.4])
+  axis.set_xticks([-0.4, -0.2, 0, 0.2, 0.4])
+  axis.set_xticklabels([-0.4, None, 0, None, 0.4])
+  axis.set_yticks([-0.4, -0.2, 0, 0.2, 0.4])
+  axis.set_yticklabels([-0.4, None, 0, None, 0.4])
 
   return axis
 
@@ -98,9 +95,13 @@ def plot_hidden_out(X, axis):
   """
   axis.scatter(np.arange(X.shape[1]), X[0], color=COLORS['hidden_out'])
   axis.set_xlabel('unit')
+  axis.set_xlim(0, 500)
+  axis.set_xticks([0, 125, 250, 375, 500])
+  axis.set_xticklabels([0, None, 250, None, 500])
   axis.set_ylabel('output weights')
-  axis.set_ylim(-0.2, 0.2)
-  axis.set_yticks([-0.2, 0, 0.2])
+  axis.set_ylim(-0.25, 0.25)
+  axis.set_yticks([-0.2, -0.1, 0, 0.1, 0.2])
+  axis.set_yticklabels([-0.2, None, 0, None, 0.2])
 
   return axis
 
@@ -109,10 +110,10 @@ def plot_3d_embedding(X, y, embedding):
   fig.set_size_inches(8, 8)
   axis = fig.add_subplot(111, projection='3d')
 
-  colors = get_task_color(X)
   for i in range(X.shape[0]):
-      marker = '^' if y[i] == 1 else 'v'
-      axis.scatter(embedding[i, 0], embedding[i, 1], embedding[i, 2], c=colors[i], marker=marker, s=200)
+      color = COLORS['pos'] if y[i] == 1 else COLORS['neg']
+      marker = 'o' if X[i, 1] == 1 else '^'
+      axis.scatter(embedding[i, 0], embedding[i, 1], embedding[i, 2], c=color, marker=marker, s=200)
 
   axis.set_xlabel('PC1')
   axis.set_ylabel('PC2')
