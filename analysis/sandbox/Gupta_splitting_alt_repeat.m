ce132dc453715e61433376bff029d72eafb60146
transedward <hungtu.e.chen@gmail.com>
2024-09-29 01:15:11 -0400

    Add Gupta ensemble splitting analysis

--- analysis/sandbox/Gupta_splitting_alt_repeat.m
+++ analysis/sandbox/Gupta_splitting_alt_repeat.m
@@ -1,5 +1,4 @@
 %%
-
 alt_total_count = NaN(1, length(out));
 L_total_count = NaN(1, length(out));
 R_total_count = NaN(1, length(out));
@@ -56,15 +55,12 @@ for s_i = 1:length(out)
         % Only use half-sessions with > 10 trials
         if block_num > 10
             if strcmp(out{s_i}.contigency{c_i}, 'Alt')
-                alt_total_count(s_i) = block_num;
                 alt_splitting(s_i) = FR_diff;
             elseif strcmp(out{s_i}.contigency{c_i}, 'Left')
-                L_total_count(s_i) = block_num;
                 if L_errs_count(s_i) > 2
                     L_splitting(s_i) = FR_diff;
                 end
             else
-                R_total_count(s_i) = block_num;
                 if R_errs_count(s_i) > 2
                     R_splitting(s_i) = FR_diff;
                 end
@@ -73,7 +69,6 @@ for s_i = 1:length(out)
     end
 end
 
-%%
 %%
 types = {'Alt', 'R or L'};
 single_splitting = {alt_splitting, [L_splitting, R_splitting]};
@@ -92,6 +87,67 @@ fs = 24;
 y_label = '$\left|FR_{left} - FR_{right}\right|$';
 
 x = dx * (1:length(single_splitting));
+xpad = 0.05;
+h = errorbar(x, mean_splittng, sem_splittng, 'LineStyle', 'none', 'LineWidth', 2);
+set(h, 'Color', 'k');
+hold on;
+plot(x, mean_splittng, '.k', 'MarkerSize', 20);
+set(gca, 'XTick', x, 'YTick', [ylim(1):dy:ylim(2)], 'XTickLabel', types, ...
+    'XLim', [x(1)-xpad x(end)+xpad], 'YLim', [ylim(1), ylim(2)], 'FontSize', fs, ...
+    'LineWidth', 1, 'TickDir', 'out');
+box off;
+plot([x(1)-xpad x(end)+xpad], [0 0], '--k', 'LineWidth', 1, 'Color', [0.7 0.7 0.7]);
+ylabel(y_label, 'Interpreter','latex');
+
+%%
+alt_ensemble_splitting = NaN(1, length(out));
+L_ensemble_splitting = NaN(1, length(out));
+R_ensemble_splitting = NaN(1, length(out));
+
+for s_i = 1:length(out)
+    switch_idx = out{s_i}.switch_idx;
+    for c_i = 1:length(out{s_i}.contigency)
+        avg_preCP_correct = (out{s_i}.left_preCP_correct{c_i} + out{s_i}.right_preCP_correct{c_i}) / 2;
+        if c_i == 1
+            block_num = length(out{s_i}.behav_sequence(1:switch_idx));
+        else
+            block_num = length(out{s_i}.behav_sequence(switch_idx+1:end));
+        end
+        % Only use half-sessions with > 10 trials
+        if block_num > 10
+            if strcmp(out{s_i}.contigency{c_i}, 'Alt')
+                alt_ensemble_splitting(s_i) = avg_preCP_correct;
+            elseif strcmp(out{s_i}.contigency{c_i}, 'Left')
+                if L_errs_count(s_i) > 2
+                    L_ensemble_splitting(s_i) = avg_preCP_correct;
+                end
+            else
+                if R_errs_count(s_i) > 2
+                    R_ensemble_splitting(s_i) = avg_preCP_correct;
+                end
+            end
+        end
+    end
+end
+
+%%
+types = {'Alt', 'R or L'};
+ensemble_splitting = {alt_ensemble_splitting, [L_ensemble_splitting, R_ensemble_splitting]};
+mean_splittng = zeros(length(ensemble_splitting), 1);
+sem_splittng = zeros(length(ensemble_splitting), 1);
+
+for d_i = 1:length(ensemble_splitting)
+    mean_splittng(d_i) = nanmean(ensemble_splitting{d_i});
+    sem_splittng(d_i) = nanstd(ensemble_splitting{d_i}) / sqrt(sum(~isnan(ensemble_splitting{d_i})));
+end
+
+ylim = [0.5, 0.7];
+dx = 0.05;
+dy = 0.05;
+fs = 24;
+y_label = 'ensemble splitter strength';
+
+x = dx * (1:length(ensemble_splitting));
 xpad = 0.05;
 h = errorbar(x, mean_splittng, sem_splittng, 'LineStyle', 'none', 'LineWidth', 2);
 set(h, 'Color', 'k');
--- analysis/sandbox/Gupta_splitting_vs_replay_bias.m
+++ analysis/sandbox/Gupta_splitting_vs_replay_bias.m
@@ -1,36 +1,38 @@
 %% Ensemble splitting
-left_preCP_correct = cellfun(@(x) x.left_preCP_correct, out);
-left_postCP_correct = cellfun(@(x) x.left_postCP_correct, out);
-right_preCP_correct = cellfun(@(x) x.right_preCP_correct, out);
-right_postCP_correct = cellfun(@(x) x.right_postCP_correct, out);
+for c_i = 1:2
+    left_preCP_correct = cellfun(@(x) x.left_preCP_correct{c_i}, out);
+    left_postCP_correct = cellfun(@(x) x.left_postCP_correct{c_i}, out);
+    right_preCP_correct = cellfun(@(x) x.right_preCP_correct{c_i}, out);
+    right_postCP_correct = cellfun(@(x) x.right_postCP_correct{c_i}, out);
 
-left_chance_pre = cellfun(@(x) x.left_chance_pre, out);
-left_chance_post = cellfun(@(x) x.left_chance_post, out);
-right_chance_pre = cellfun(@(x) x.right_chance_pre, out);
-right_chance_post = cellfun(@(x) x.right_chance_post, out);
+    left_chance_pre = cellfun(@(x) x.left_chance_pre{c_i}, out);
+    left_chance_post = cellfun(@(x) x.left_chance_post{c_i}, out);
+    right_chance_pre = cellfun(@(x) x.right_chance_pre{c_i}, out);
+    right_chance_post = cellfun(@(x) x.right_chance_post{c_i}, out);
 
-ywhats = {left_preCP_correct, left_postCP_correct, right_preCP_correct, right_postCP_correct};
-ychances = {left_chance_pre, left_chance_post, right_chance_pre, right_chance_post};
-cols = 'rrbb';
-xd = [1 2 4 5];
+    ywhats = {left_preCP_correct, left_postCP_correct, right_preCP_correct, right_postCP_correct};
+    ychances = {left_chance_pre, left_chance_post, right_chance_pre, right_chance_post};
+    cols = 'rrbb';
+    xd = [1 2 4 5];
 
-figure;
+    figure;
 
-for iW = 1:length(ywhats)
-    
-   ydata = ywhats{iW}; ychance = ychances{iW};
-   h = bar(xd(iW),nanmean(ydata)); set(h,'FaceColor',cols(iW),'EdgeColor','none','BarWidth',0.6);
-   hold on;
-   h2 = plot(repmat(xd(iW),[1 length(ydata)]),ydata,'.k');
-   h3 = plot([xd(iW)-0.4 xd(iW)+0.4],[nanmean(ychance) nanmean(ychance)],':k','LineWidth',2);
-    
-   % report means and SEMs; statistics vs. chance
-   m = nanmean(ydata); s = nanstd(ydata)./sqrt(4); p = ranksum(ydata,ychance);
-   fprintf('%s: %.2f +/- %.2f (SEM), p = %2.2e vs. chance\n',ywhats{iW},m,s,p);
-   
+    for iW = 1:length(ywhats)
+
+        ydata = ywhats{iW}; ychance = ychances{iW};
+        h = bar(xd(iW),nanmean(ydata)); set(h,'FaceColor',cols(iW),'EdgeColor','none','BarWidth',0.6);
+        hold on;
+        h2 = plot(repmat(xd(iW),[1 length(ydata)]),ydata,'.k');
+        h3 = plot([xd(iW)-0.4 xd(iW)+0.4],[nanmean(ychance) nanmean(ychance)],':k','LineWidth',2);
+
+        % report means and SEMs; statistics vs. chance
+        m = nanmean(ydata); s = nanstd(ydata)./sqrt(4); p = ranksum(ydata,ychance);
+        fprintf('%s: %.2f +/- %.2f (SEM), p = %2.2e vs. chance\n',ywhats{iW},m,s,p);
+
+    end
+    set(gca,'LineWidth',1,'TickDir','out','FontSize',18,'XLim',[0 6],'XTick',xd,'XTickLabel',{'preL','postL','preR','postR'},'YLim',[0 1],'YTick',0:0.25:1);
+    box off;
 end
-set(gca,'LineWidth',1,'TickDir','out','FontSize',18,'XLim',[0 6],'XTick',xd,'XTickLabel',{'preL','postL','preR','postR'},'YLim',[0 1],'YTick',0:0.25:1);
-box off;
 
 %% Single-cell splitting
 FR_diff = cellfun(@(x) mean(x.left_preCP_correct, 'omitnan'), out);
