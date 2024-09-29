0d865345abe47299171fa885d1fb4a84c48ce517
Hung-Tu Chen <hungtu.e.chen@gmail.com>
2024-09-29 01:12:07 -0400

    Update GuptaMatchedTrials and location decodeing

--- analysis/utils/data/Get_Gupta_DecSeqCombined.m
+++ analysis/utils/data/Get_Gupta_DecSeqCombined.m
@@ -45,6 +45,7 @@ cfg_tc.removeInterneurons = 1;
 cfg_tc.interval = [0, metadata.TimeOffTrack];
 TC = get_tuning_curve(cfg_tc, data_path);
 
+cfg_tc.use_matched_trials = 1;
 if metadata.SwitchTime > 0
     cfg_tc.interval = [0, metadata.SwitchTime];
 end
@@ -67,8 +68,8 @@ out.trial_iv = metadata.taskvars_err.trial_iv;
 out.TimeOffTrack = metadata.TimeOffTrack;
 
 %% Get firing rates for left and right trials before the choice points
-for b_i = 1:2
-    if b_i == 1
+for c_i = 1:2
+    if c_i == 1
         trial_iv_L = restrict(metadata.taskvars_err.trial_iv_L, 0, metadata.SwitchTime);
         trial_iv_R = restrict(metadata.taskvars_err.trial_iv_R, 0, metadata.SwitchTime);
     else
@@ -80,7 +81,7 @@ for b_i = 1:2
     trial_nums = L_nums + R_nums;
     avg_FR_per_trial = NaN(length(TC.combined.S.t), trial_nums);
     
-    if b_i == 1
+    if c_i == 1
         all_TCs = {TC_1st.left, TC_1st.right};
     else
         all_TCs = {TC_2nd.left, TC_2nd.right};
@@ -110,7 +111,7 @@ for b_i = 1:2
     L_FR = mean(avg_FR_per_trial(:, 1:L_nums), 2, 'omitnan');
     R_FR = mean(avg_FR_per_trial(:, L_nums+1:end), 2, 'omitnan');
     FR_diff = abs(L_FR - R_FR);
-    out.FR_diff{b_i} = FR_diff;
+    out.FR_diff{c_i} = FR_diff;
     % figure; imagesc([L_FR, R_FR, FR_diff]); colorbar;
 end
 
@@ -121,47 +122,60 @@ cfg_Q.boxcar_size = cfg.Qboxcar;
 cfg_Q.smooth = [];
 
 Q = MakeQfromS(cfg_Q, decS);
+Q = restrict(Q, 0, metadata.TimeOffTrack);
+if metadata.SwitchTime > 0
+    Q_1st = restrict(Q, 0, metadata.SwitchTime);
+else
+    Q_1st = Q;
+end 
+Q_2nd = restrict(Q, metadata.SwitchTime, metadata.TimeOffTrack);
 
 %% decode
 clear csc pos S;
 cfg_decode = [];
 cfg_decode.nMinSpikes = cfg.dt;
 cfg_decode.excludeMethod = 'frate';
-P = DecodeZ(cfg_decode,Q,TC.combined.tc.tc); % full decoded probability distribution
+% P = DecodeZ(cfg_decode,Q,TC.combined.tc.tc);
+P_1st = DecodeZ(cfg_decode,Q_1st,TC_1st.combined.tc.tc); % full decoded probability distribution
+P_2nd = DecodeZ(cfg_decode,Q_2nd,TC_2nd.combined.tc.tc);
 
 %% quantify decoding accuracy on RUN
-this_trueZ = tsd(TC.combined.linpos.tvec,TC.combined.tc.usr.pos_idx); % true position in units of bins (as established by tuning curves)
-cfg_err = []; cfg_err.mode = 'max';
-
-keep_idx = unique(nearest_idx3(this_trueZ.tvec,P.tvec)); % match up decoding with true positions
-this_Pscore = P;
-this_Pscore.tvec = this_Pscore.tvec(keep_idx);
-this_Pscore.data = this_Pscore.data(:,keep_idx);
-[Perr, confMat] = DecodeErrorZ(cfg_err,this_Pscore,this_trueZ);
+Ps = {P_1st, P_2nd};
+TCs = {TC_1st, TC_2nd};
+for c_i = 1:2
+    this_trueZ = tsd(TCs{c_i}.combined.linpos.tvec,TCs{c_i}.combined.tc.usr.pos_idx); % true position in units of bins (as established by tuning curves)
+    cfg_err = []; cfg_err.mode = 'max';
+    keep_idx = unique(nearest_idx3(this_trueZ.tvec,Ps{c_i}.tvec)); % match up decoding with true positions
+    this_Pscore = Ps{c_i};
+    this_Pscore.tvec = this_Pscore.tvec(keep_idx);
+    this_Pscore.data = this_Pscore.data(:,keep_idx);
+    [~, confMat{c_i}] = DecodeErrorZ(cfg_err,this_Pscore,this_trueZ);
+end
 
 %% compare quadrants
-cfy = @(x) x(:);
-this_mat = confMat.thr;
-
-keep = ~isnan(nansum(confMat.thr')); % find non-nan columns
-left_pts = [1 TC.left.cp_bin TC.combined.nBins/2]; np_l1 = sum(keep(left_pts(1):left_pts(2))); np_l2 = sum(keep(left_pts(2)+1:left_pts(3)));
-right_pts = [TC.combined.nBins/2+1 TC.combined.nBins/2+TC.right.cp_bin, TC.combined.nBins]; np_r1 = sum(keep(right_pts(1):right_pts(2))); np_r2 = sum(keep(right_pts(2)+1:right_pts(3)));
-
-out.left_preCP_correct = nansum(cfy(this_mat(left_pts(1):left_pts(2),left_pts(1):left_pts(2))))./np_l1;
-out.left_preCP_incorrect = nansum(cfy(this_mat(left_pts(1):left_pts(2),right_pts(1):right_pts(2))))./np_l1; % points classified as OPPOSITE trial equivalent
-out.right_preCP_correct = nansum(cfy(this_mat(right_pts(1):right_pts(2),right_pts(1):right_pts(2))))./np_r1;
-out.right_preCP_incorrect = nansum(cfy(this_mat(right_pts(1):right_pts(2),left_pts(1):left_pts(2))))./np_r1;
-fprintf('PreCP: left+ %.3f, left- %.3f, right+ %.3f, right- %.3f\n',out.left_preCP_correct,out.left_preCP_incorrect,out.right_preCP_correct,out.right_preCP_incorrect);
-
-out.left_postCP_correct = nansum(cfy(this_mat(left_pts(2)+1:left_pts(3),left_pts(2)+1:left_pts(3))))./np_l2;
-out.left_postCP_incorrect = nansum(cfy(this_mat(left_pts(2)+1:left_pts(3),right_pts(2)+1:right_pts(3))))./np_l2;
-out.right_postCP_correct = nansum(cfy(this_mat(right_pts(2)+1:right_pts(3),right_pts(2)+1:right_pts(3))))./np_r2;
-out.right_postCP_incorrect = nansum(cfy(this_mat(right_pts(2)+1:right_pts(3),left_pts(2)+1:left_pts(3))))./np_r2;
-fprintf('PostCP: left+ %.3f, left- %.3f, right+ %.3f, right- %.3f\n',out.left_postCP_correct,out.left_postCP_incorrect,out.right_postCP_correct,out.right_postCP_incorrect);
-
-out.left_chance_pre = np_l1./(np_l1+np_l2+np_r1+np_r2); out.left_chance_post = np_l2./(np_l1+np_l2+np_r1+np_r2); % analytical chance levels based on number of bins
-out.right_chance_pre = np_r1./(np_l1+np_l2+np_r1+np_r2); out.right_chance_post = np_r2./(np_l1+np_l2+np_r1+np_r2); % analytical chance levels based on number of bins
-
+for c_i = 1:2
+    cfy = @(x) x(:);
+    this_mat = confMat{c_i}.thr;
+    
+    keep = ~isnan(nansum(confMat{c_i}.thr')); % find non-nan columns
+    left_pts = [1 TCs{c_i}.left.cp_bin TC.combined.nBins/2]; np_l1 = sum(keep(left_pts(1):left_pts(2))); np_l2 = sum(keep(left_pts(2)+1:left_pts(3)));
+    right_pts = [TC.combined.nBins/2+1 TC.combined.nBins/2+TCs{c_i}.right.cp_bin, TC.combined.nBins]; np_r1 = sum(keep(right_pts(1):right_pts(2))); np_r2 = sum(keep(right_pts(2)+1:right_pts(3)));
+    
+    out.left_preCP_correct{c_i} = nansum(cfy(this_mat(left_pts(1):left_pts(2),left_pts(1):left_pts(2))))./np_l1;
+    out.left_preCP_incorrect{c_i} = nansum(cfy(this_mat(left_pts(1):left_pts(2),right_pts(1):right_pts(2))))./np_l1; % points classified as OPPOSITE trial equivalent
+    out.right_preCP_correct{c_i} = nansum(cfy(this_mat(right_pts(1):right_pts(2),right_pts(1):right_pts(2))))./np_r1;
+    out.right_preCP_incorrect{c_i} = nansum(cfy(this_mat(right_pts(1):right_pts(2),left_pts(1):left_pts(2))))./np_r1;
+    fprintf('PreCP: left+ %.3f, left- %.3f, right+ %.3f, right- %.3f\n',out.left_preCP_correct{c_i},out.left_preCP_incorrect{c_i},out.right_preCP_correct{c_i},out.right_preCP_incorrect{c_i});
+    
+    out.left_postCP_correct{c_i} = nansum(cfy(this_mat(left_pts(2)+1:left_pts(3),left_pts(2)+1:left_pts(3))))./np_l2;
+    out.left_postCP_incorrect{c_i} = nansum(cfy(this_mat(left_pts(2)+1:left_pts(3),right_pts(2)+1:right_pts(3))))./np_l2;
+    out.right_postCP_correct{c_i} = nansum(cfy(this_mat(right_pts(2)+1:right_pts(3),right_pts(2)+1:right_pts(3))))./np_r2;
+    out.right_postCP_incorrect{c_i} = nansum(cfy(this_mat(right_pts(2)+1:right_pts(3),left_pts(2)+1:left_pts(3))))./np_r2;
+    fprintf('PostCP: left+ %.3f, left- %.3f, right+ %.3f, right- %.3f\n',out.left_postCP_correct{c_i},out.left_postCP_incorrect{c_i},out.right_postCP_correct{c_i},out.right_postCP_incorrect{c_i});
+    
+    out.left_chance_pre{c_i} = np_l1./(np_l1+np_l2+np_r1+np_r2); out.left_chance_post{c_i} = np_l2./(np_l1+np_l2+np_r1+np_r2); % analytical chance levels based on number of bins
+    out.right_chance_pre{c_i} = np_r1./(np_l1+np_l2+np_r1+np_r2); out.right_chance_post{c_i} = np_r2./(np_l1+np_l2+np_r1+np_r2); % analytical chance levels based on number of bins
+end
 %% now decode SWR vectors
 %
 cfg_shuf_LR.dt = 0.05; % this sets the binsize in decoding
